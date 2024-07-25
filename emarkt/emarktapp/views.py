155d1b65a390b749b6b360332ca020a98e926327
tk <kauati.toiti@gmail.com>
2024-07-25 17:12:01 +0100

    views and template (basic)

--- emarkt/emarktapp/views.py
+++ emarkt/emarktapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 def greet(request):
-    return HttpResponse('Hello World')
+    return render(request, 'greet.html')
