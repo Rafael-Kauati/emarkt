9f930a74429fe87b2d38d3ef72a814e985b5edaa
tk <kauati.toiti@gmail.com>
2024-07-31 14:59:41 +0100

    Sing in restriction messages

--- emarkt/emarktapp/forms.py
+++ emarkt/emarktapp/forms.py
@@ -1,7 +1,7 @@
-# emarktapp/forms.py
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,6 +9,18 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email address is already in use.")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already taken.")
+        return username
 
     def save(self, commit=True):
         user = super(SignUpForm, self).save(commit=False)
