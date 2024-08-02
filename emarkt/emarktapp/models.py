e4a1c17ed247c7289cea28fb051a900e72bdcb44
tk <kauati.toiti@gmail.com>
2024-08-02 15:49:25 +0100

    Product owner update

--- emarkt/emarktapp/models.py
+++ emarkt/emarktapp/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
-    owner = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
